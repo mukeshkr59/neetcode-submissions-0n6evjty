0f1f7c48e552216655471a73b99d30f041866ded
Mukesh Kumar Vishwakarma <91455525+mukeshkr59@users.noreply.github.com>
2026-04-15 17:59:49 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int b = INT_MAX;
+        int sell = 0;
+        int profit = 0;
+        for( int i = 0;i<n;i++){
+            if( prices[i]<b){
+                b = prices[i];
+            }
+            // b = min(b, prices[i]);
+            profit = max( profit, prices[i]-b);          
+        }
+        return profit;
+    }
+};
