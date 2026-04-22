39364f348f12c2712c75f89ddaa31befb500828e
Mukesh Kumar Vishwakarma <91455525+mukeshkr59@users.noreply.github.com>
2026-04-22 17:25:23 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        if( n > s2.size()) return false;
+        int j = 0;
+        unordered_map<char, int> mpp1;
+        unordered_map<char, int> mpp2;
+        for( auto c : s1){
+            mpp1[c]++;
+        }
+        for( int i= 0;i<n;i++){
+            mpp2[s2[i]]++;
+        }
+        if( mpp1 == mpp2) return true;
+        for( int i = n; i<s2.size();i++){
+            mpp2[s2[j]]--;
+            if(mpp2[s2[j]] == 0){
+                mpp2.erase(s2[j]);
+            }
+            j++;
+            mpp2[s2[i]]++;
+            if(mpp1 == mpp2 ){
+                return true;
+            }
+        }
+        return false;
+    }
+};
